5db6b2b372e294a2ef4ce15e96d99ae0f0416873
Gabriel Vieira <153781259+tiobubu@users.noreply.github.com>
2024-01-03 10:14:22 -0300

    Add files via upload

---
+++ adivinhacao.py
@@ -0,0 +1,18 @@
+import random
+
+def jogo_adivinhacao():
+    numale = random.randint(1, 100)
+    tentativas = 0
+
+    while True:
+        tentativa = int(input('Tente adivinhar o número entre 1 e 100: '))
+        tentativas += 1
+
+        if tentativa < numale: print('Tente um número maior.')
+        elif tentativa > numale: print('Tente um número menor.')
+        else:
+            print(f'Parabéns! Você acertou o número {numale} em {tentativas} tentativas')
+            break
+
+if __name__ == '__main__':
+    jogo_adivinhacao()
